fa40af0befb00955d99b1c3fc407163969e88ae2
Alexander Stensrud <alex@yuki.no>
2021-04-04 17:11:55 +0200

    Plugins

---
+++ lua/plugin/buftabline.lua
@@ -0,0 +1,46 @@
+local util = require('junkbox.util')
+
+vim.cmd [[highlight TabLineFill guifg=#000000  guibg=#444444]]
+vim.cmd [[highlight TabLine guifg=#222222 guibg=#666666]]
+vim.cmd [[highlight TabLineSel guibg=#928374 guifg=#000000 gui=bold]]
+
+util.lnmap('1', [[:lua require('buftabline').go_to_buffer(1)<cr>]])
+util.lnmap('2', [[:lua require('buftabline').go_to_buffer(2)<cr>]])
+util.lnmap('3', [[:lua require('buftabline').go_to_buffer(3)<cr>]])
+util.lnmap('4', [[:lua require('buftabline').go_to_buffer(4)<cr>]])
+util.lnmap('5', [[:lua require('buftabline').go_to_buffer(5)<cr>]])
+util.lnmap('6', [[:lua require('buftabline').go_to_buffer(6)<cr>]])
+util.lnmap('7', [[:lua require('buftabline').go_to_buffer(7)<cr>]])
+util.lnmap('8', [[:lua require('buftabline').go_to_buffer(8)<cr>]])
+util.lnmap('9', [[:lua require('buftabline').go_to_buffer(9)<cr>]])
+
+util.lnmap('k1', [[:lua require('buftabline').kill_buffer(1)<cr>]])
+util.lnmap('k2', [[:lua require('buftabline').kill_buffer(2)<cr>]])
+util.lnmap('k3', [[:lua require('buftabline').kill_buffer(3)<cr>]])
+util.lnmap('k4', [[:lua require('buftabline').kill_buffer(4)<cr>]])
+util.lnmap('k5', [[:lua require('buftabline').kill_buffer(5)<cr>]])
+util.lnmap('k6', [[:lua require('buftabline').kill_buffer(6)<cr>]])
+util.lnmap('k7', [[:lua require('buftabline').kill_buffer(7)<cr>]])
+util.lnmap('k8', [[:lua require('buftabline').kill_buffer(8)<cr>]])
+util.lnmap('k9', [[:lua require('buftabline').kill_buffer(9)<cr>]])
+
+util.lnmap('v1', [[:lua require('buftabline').custom_command(1)<cr>]])
+util.lnmap('v2', [[:lua require('buftabline').custom_command(2)<cr>]])
+util.lnmap('v3', [[:lua require('buftabline').custom_command(3)<cr>]])
+util.lnmap('v4', [[:lua require('buftabline').custom_command(4)<cr>]])
+util.lnmap('v5', [[:lua require('buftabline').custom_command(5)<cr>]])
+util.lnmap('v6', [[:lua require('buftabline').custom_command(6)<cr>]])
+util.lnmap('v7', [[:lua require('buftabline').custom_command(7)<cr>]])
+util.lnmap('v8', [[:lua require('buftabline').custom_command(8)<cr>]])
+util.lnmap('v9', [[:lua require('buftabline').custom_command(9)<cr>]])
+
+require("buftabline").setup{
+  index_format = '%d-',
+  custom_command = 'vertical sb',
+  go_to_maps = false,
+  kill_maps = false,
+  highlight_hidden = true,
+  hlgroup_current = 'TabLineSel',
+  hlgroup_normal = 'TabLine',
+  hlgroup_hidden = 'TabLineFill',
+}
--- lua/plugin/packer.lua
+++ lua/plugin/packer.lua
@@ -13,93 +13,71 @@ return require('packer').startup(function()
     },
   }
 
-  use { 'mhinz/vim-sayonara', cmd = 'Sayonara' }
-
-  use { 'dstein64/vim-startuptime', cmd = 'StartupTime' }
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    config = function() require('plugin.treesitter') end,
-    run = function() vim.cmd('TSUpdate') end,
-  }
-
-  use {
-    'psliwka/vim-smoothie',
-    config = function() require('plugin.smoothie') end,
-  }
-
-  use {
-    'fatih/vim-go',
-    ft = { 'go' },
-    config = function() require('plugin.go') end,
-  }
-
-  use {
-    'hrsh7th/nvim-compe',
-    config = function() require('plugin.compe') end,
-  }
-
-  use {
-    'hoob3rt/lualine.nvim',
-    config = function() require('plugin.lualine') end,
-  }
-
-  use {
-    'liuchengxu/vim-which-key',
-    config = function() require('plugin.which-key') end,
-  }
-
-  use {
-    'neovim/nvim-lspconfig',
-    config = function() require('plugin.lspconfig') end,
-  }
-
-  use 'romainl/vim-cool'
-
   use {
     'spektroskop/golden-ratio',
     config = function() require('plugin.golden-ratio') end,
-  }
-
-  use {
-    'sheerun/vim-polyglot',
-    config = function() require('plugin.polyglot') end,
-  }
-
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-unimpaired'
-  use 'tpope/vim-vinegar'
-
-  use {
-    'npxbr/gruvbox.nvim',
-    requires = { 'rktjmp/lush.nvim' },
-  }
-
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
-    config = function() require('plugin.telescope') end,
+    branch = 'skip-neovim-floating-windows',
   }
 
   use {
     'spektroskop/buftabline.nvim',
+    config = function() require('plugin.buftabline') end,
     branch = 'highlight-hidden',
-    config = function()
-      vim.cmd [[highlight TabLineFill guifg=#000000  guibg=#444444]]
-      vim.cmd [[highlight TabLine guifg=#222222 guibg=#666666]]
-      vim.cmd [[highlight TabLineSel guibg=#928374 guifg=#000000 gui=bold]]
-
-      require("buftabline").setup{
-        go_to_maps = true,
-        kill_maps = true,
-        highlight_hidden = true,
-        hlgroup_current = 'TabLineSel',
-        hlgroup_normal = 'TabLine',
-        hlgroup_hidden = 'TabLineFill',
-      }
-    end,
   }
+
+  use { 'dstein64/vim-startuptime', cmd = 'StartupTime' }
+
+  use { 'fatih/vim-go',
+    ft = { 'go' },
+    config = function() require('plugin.go') end,
+  }
+
+  use { 'hoob3rt/lualine.nvim',
+    config = function() require('plugin.lualine') end,
+  }
+
+  use { 'hrsh7th/nvim-compe',
+    config = function() require('plugin.compe') end,
+  }
+
+  use { 'liuchengxu/vim-which-key',
+    config = function() require('plugin.which-key') end,
+  }
+
+  use { 'mhinz/vim-sayonara', cmd = 'Sayonara' }
+
+  use { 'neovim/nvim-lspconfig',
+    config = function() require('plugin.lspconfig') end,
+  }
+
+  use { 'npxbr/gruvbox.nvim',
+    requires = { 'rktjmp/lush.nvim' },
+  }
+
+  use { 'nvim-telescope/telescope.nvim',
+    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
+    config = function() require('plugin.telescope') end,
+  }
+
+  use { 'nvim-treesitter/nvim-treesitter',
+    config = function() require('plugin.treesitter') end,
+    run = function() vim.cmd('TSUpdate') end,
+  }
+
+  use { 'psliwka/vim-smoothie',
+    config = function() require('plugin.smoothie') end,
+  }
+
+  use { 'romainl/vim-cool' }
+
+  use { 'sheerun/vim-polyglot',
+    config = function() require('plugin.polyglot') end,
+  }
+
+  use { 'tpope/vim-commentary' }
+  use { 'tpope/vim-fugitive' }
+  use { 'tpope/vim-repeat' }
+  use { 'tpope/vim-surround' }
+  use { 'tpope/vim-unimpaired' }
+  use { 'tpope/vim-vinegar' }
 end)
