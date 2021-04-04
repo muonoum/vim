68daeae1659a5225e1f316bfe5d002216deb3db7
Alexander Stensrud <alex@yuki.no>
2021-04-04 19:20:16 +0200

    Reformat plugin/packer

--- lua/plugin/packer.lua
+++ lua/plugin/packer.lua
@@ -1,83 +1,91 @@
-return require('packer').startup(function()
-  use {
-    'wbthomason/packer.nvim',
-    opt = true,
-    config = function() require('plugin.packer') end,
-    cmd = {
-      'PackerClean',
-      'PackerCompile',
-      'PackerInstall',
-      'PackerStatus',
-      'PackerSync',
-      'PackerUpdate'
-    },
-  }
+return require('packer').startup(
+  function(use)
+    use {
+      'wbthomason/packer.nvim',
+      opt = true,
+      config = function() require('plugin.packer') end,
+      cmd = {
+        'PackerClean',
+        'PackerCompile',
+        'PackerInstall',
+        'PackerStatus',
+        'PackerSync',
+        'PackerUpdate'
+      },
+    }
 
-  use {
-    'spektroskop/golden-ratio',
-    config = function() require('plugin.golden-ratio') end,
-    branch = 'skip-neovim-floating-windows',
-  }
+    use {
+      'spektroskop/golden-ratio',
+      config = function() require('plugin.golden-ratio') end,
+      branch = 'skip-neovim-floating-windows',
+    }
 
-  use {
-    'spektroskop/buftabline.nvim',
-    config = function() require('plugin.buftabline') end,
-    branch = 'highlight-hidden',
-  }
+    use {
+      'spektroskop/buftabline.nvim',
+      config = function() require('plugin.buftabline') end,
+      branch = 'highlight-hidden',
+    }
 
-  use { 'dstein64/vim-startuptime', cmd = 'StartupTime' }
+    use { 'dstein64/vim-startuptime',
+      cmd = 'StartupTime',
+    }
 
-  use { 'fatih/vim-go',
-    ft = { 'go' },
-    config = function() require('plugin.go') end,
-  }
+    use { 'fatih/vim-go',
+      ft = { 'go' },
+      config = function() require('plugin.go') end,
+    }
 
-  use { 'hoob3rt/lualine.nvim',
-    config = function() require('plugin.lualine') end,
-  }
+    use { 'hoob3rt/lualine.nvim',
+      config = function() require('plugin.lualine') end,
+    }
 
-  use { 'hrsh7th/nvim-compe',
-    config = function() require('plugin.compe') end,
-  }
+    use { 'hrsh7th/nvim-compe',
+      config = function() require('plugin.compe') end,
+    }
 
-  use { 'liuchengxu/vim-which-key',
-    config = function() require('plugin.which-key') end,
-  }
+    use { 'liuchengxu/vim-which-key',
+      config = function() require('plugin.which-key') end,
+    }
 
-  use { 'mhinz/vim-sayonara', cmd = 'Sayonara' }
+    use { 'mhinz/vim-sayonara', cmd = 'Sayonara' }
 
-  use { 'neovim/nvim-lspconfig',
-    config = function() require('plugin.lspconfig') end,
-  }
+    use { 'neovim/nvim-lspconfig',
+      config = function() require('plugin.lspconfig') end,
+    }
 
-  use { 'npxbr/gruvbox.nvim',
-    requires = { 'rktjmp/lush.nvim' },
-  }
+    use { 'npxbr/gruvbox.nvim',
+      requires = { 'rktjmp/lush.nvim' },
+    }
 
-  use { 'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
-    config = function() require('plugin.telescope') end,
-  }
+    use { 'nvim-telescope/telescope.nvim',
+      requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
+      config = function() require('plugin.telescope') end,
+    }
 
-  use { 'nvim-treesitter/nvim-treesitter',
-    config = function() require('plugin.treesitter') end,
-    run = function() vim.cmd('TSUpdate') end,
-  }
+    use { 'nvim-treesitter/nvim-treesitter',
+      config = function() require('plugin.treesitter') end,
+      run = function() vim.cmd('TSUpdate') end,
+    }
 
-  use { 'psliwka/vim-smoothie',
-    config = function() require('plugin.smoothie') end,
-  }
+    use { 'psliwka/vim-smoothie',
+      config = function() require('plugin.smoothie') end,
+    }
 
-  use { 'romainl/vim-cool' }
+    use { 'romainl/vim-cool' }
 
-  use { 'sheerun/vim-polyglot',
-    config = function() require('plugin.polyglot') end,
-  }
+    use { 'sheerun/vim-polyglot',
+      config = function() require('plugin.polyglot') end,
+    }
 
-  use { 'tpope/vim-commentary' }
-  use { 'tpope/vim-fugitive' }
-  use { 'tpope/vim-repeat' }
-  use { 'tpope/vim-surround' }
-  use { 'tpope/vim-unimpaired' }
-  use { 'tpope/vim-vinegar' }
-end)
+    use { 'norcalli/nvim-colorizer.lua',
+      config = function() require'colorizer'.setup() end,
+    }
+
+    use { 'tpope/vim-commentary' }
+    use { 'tpope/vim-fugitive' }
+    use { 'tpope/vim-repeat' }
+    use { 'tpope/vim-surround' }
+    use { 'tpope/vim-unimpaired' }
+    use { 'tpope/vim-vinegar' }
+  end
+)
