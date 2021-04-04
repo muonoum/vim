3f47cba9561daa28c8f658e143fc78ac4042d924
Alexander Stensrud <alex@yuki.no>
2021-04-04 20:20:45 +0200

    Disable golden ratio in diff mode

--- lua/plugin/golden-ratio.lua
+++ lua/plugin/golden-ratio.lua
@@ -1,3 +1,1 @@
--- vim.g.golden_ratio_autocommand = 0
--- vim.g.golden_ratio_exclude_nonmodifiable = 1
--- if &diff | let g:loaded_golden_ratio=0 | endif
+vim.cmd [[if &diff | let g:loaded_golden_ratio=0 | endif]]
